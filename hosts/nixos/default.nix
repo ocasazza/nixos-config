b86720cd61dfdb2c2d5ae0e56596a39bce4379a4
Olive Casazza <olive.casazza@schrodinger.com>
2025-01-19 13:58:31 -0800

    forked and configured for ocasazza

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 let
-  user = "nason";
+  user = "casazza";
   keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKoLhJuOE878n9BaTFAAmGgmGjztT61HsMRJOU+uKf/t+pJLxUOn3Or2CLMG5EkfKiTZzLFRQ9y1IvHPvmrM5QB5obJP6mJm2xNlL6wmDBKF0qpcXCU5nX3SmFJdbLg5a4FRWLSdMifWK75kvOSBskTYv81W5ncsbRdHK67AciarHYbkPoktoJpJE4EpEPMrPGLS7AaRo1zfbrIfOJJc4LzX2jBzNg1gw0/iPX39KPB/F+N6DzEh8cd43B3dKlqHscHCerpsHVF0EIgFkGm76MrgoJO92qAjeln9ibVSjU9ysS0YP7Z5khyyd19HQFiMQ6Dvp5cmUxndgvKdHooGE/"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA64cq4cWGIrpV88M4x4qRkgZNdceDLRY5L27qu7Rs2f olive.casazza@schrodinger.com"
   ];
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
 in
