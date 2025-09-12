297cc1b17e72c7cc6ea276c64c8461394a1fb5b1
Olive Casazza <olive.casazza@schrodinger.com>
2025-09-12 15:26:24 -0700

    some fixes for ghostty

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -111,11 +111,11 @@
       term = "xterm-256color";
       macos-titlebar-proxy-icon = "hidden";
       config-file = "~/.config/ghostty/extra"; # for testing shaders atm
-      command = "/etc/profiles/per-user/casazza/bin/zsh";
+      command = "/Users/${user.name}/.nix-profile/bin/zsh";
       keybind = [
         "cmd+shift+d=close_surface"
-        "cmd+shift+o=new_split:down"
-        "cmd+shift+e=new_split:right"
+        "cmd+shift+e=new_split:down"
+        "cmd+shift+o=new_split:right"
       ];
     };
   };
@@ -207,127 +207,34 @@
     };
   };
 
-  #vim = {
-  #  enable = true;
-  #  plugins = with pkgs.vimPlugins; [
-  #    vim-airline
-  #    vim-airline-themes
-  #    vim-startify
-  #    vim-tmux-navigator
-  #  ];
-  #  settings = {
-  #    ignorecase = true;
-  #  };
-  #  extraConfig = ''
-  #    "" General
-  #    set number
-  #    set history=1000
-  #    set nocompatible
-  #    set modelines=0
-  #    set encoding=utf-8
-  #    set scrolloff=3
-  #    set showmode
-  #    set showcmd
-  #    set hidden
-  #    set wildmenu
-  #    set wildmode=list:longest
-  #    set cursorline
-  #    set ttyfast
-  #    set nowrap
-  #    set ruler
-  #    set backspace=indent,eol,start
-  #    set laststatus=2
-  #    set clipboard=autoselect
-
-  #    " Dir stuff
-  #    set nobackup
-  #    set nowritebackup
-  #    set noswapfile
-  #    set backupdir=~/.config/vim/backups
-  #    set directory=~/.config/vim/swap
-
-  #    "" Whitespace rules
-  #    set tabstop=8
-  #    set shiftwidth=2
-  #    set softtabstop=2
-  #    set expandtab
-
-  #    "" Searching
-  #    set incsearch
-  #    set gdefault
-
-  #    "" Statusbar
-  #    set nocompatible " Disable vi-compatibility
-  #    set laststatus=2 " Always show the statusline
-  #    let g:airline_theme='seoul256'
-  #    let g:airline_powerline_fonts = 1
-
-  #    "" Local keys and such
-  #    let mapleader=","
-  #    let maplocalleader=" "
-
-  #    "" Change cursor on mode
-  #    :autocmd InsertEnter * set cul
-  #    :autocmd InsertLeave * set nocul
-
-  #    "" File-type highlighting and configuration
-  #    syntax on
-  #    filetype on
-  #    filetype plugin on
-  #    filetype indent on
-
-  #    "" Paste from clipboard
-  #    nnoremap <Leader>, "+gP
-
-  #    "" Copy from clipboard
-  #    xnoremap <Leader>. "+y
-
-  #    "" Move cursor by display lines when wrapping
-  #    nnoremap j gj
-  #    nnoremap k gk
-
-  #    "" Map leader-q to quit out of window
-  #    nnoremap <leader>q :q<cr>
-
-  #    "" Move around split
-  #    nnoremap <C-h> <C-w>h
-  #    nnoremap <C-j> <C-w>j
-  #    nnoremap <C-k> <C-w>k
-  #    nnoremap <C-l> <C-w>l
-
-  #    let g:startify_lists = [
-  #      \ { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
-  #      \ { 'type': 'sessions',  'header': ['   Sessions']       },
-  #      \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      }
-  #      \ ]
-
-  #    let g:startify_bookmarks = [
-  #      \ '~/.local/share/src',
-  #      \ ]
-
-  #  '';
-  #};
-
   ssh = {
     enable = true;
-    matchBlocks = {
-      "*" = {
-        extraOptions = lib.mkMerge [
-          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-            IdentityFile = "/home/${user.name}/.ssh/id_ed25519";
-          })
-          (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-            IdentityFile = "/Users/${user.name}/.ssh/id_ed25519";
-          })
-        ];
-      };
-      "github.com" = {
-        user = "${user.name}";
-        hostname = "github.com";
-        identitiesOnly = true;
-      };
-    };
     enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
+    extraConfig = lib.mkMerge [
+      ''
+        Host github.com
+          Hostname github.com
+          IdentitiesOnly yes
+      ''
+      (lib.mkIf pkgs.stdenv.hostPlatform.isLinux ''
+        IdentityFile /home/${user.name}/.ssh/id_github
+      '')
+      (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin ''
+        IdentityFile /Users/${user.name}/.ssh/id_rsa
+      '')
+    ];
   };
 }
 // import ./vscode { inherit pkgs lib; }
