3b90747a33fdac0e5ba17d3ba7202e163e33cfc4
Olive Casazza <olive.casazza@schrodinger.com>
2026-04-05 21:03:25 -0700

    feat: add exo cachix binary cache

--- modules/shared/cachix/default.nix
+++ modules/shared/cachix/default.nix
@@ -4,11 +4,13 @@ let
     "https://ocasazza.cachix.org"
     "https://na-son.cachix.org"
     "https://nix-community.cachix.org"
+    "https://exo.cachix.org"
   ];
   trustedPublicKeys = [
     "ocasazza.cachix.org-1:4J9/Csix7SSPiUIyaSeISIT475va14uZPwJVipSDY+Y="
     "na-son.cachix.org-1:CM2NirYn93VKnwoRppqqwbb6XjulYKRTcHsAbVyEpcQ="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "exo.cachix.org-1:okq7hl624TBeAR3kV+g39dUFSiaZgLRkLsFBCuJ2NZI="
   ];
 in
 {
